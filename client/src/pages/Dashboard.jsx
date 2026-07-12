6f7336036fc5e80ecc499cf6c0ed97fb2351ba46
pankaj choudhary <pankajkhicharabc3@gmail.com>
2026-07-12 15:28:50 +0530

    Add comment to Dashboard component
    Added a comment to describe the Dashboard component.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+// This is the DashBoard of the TransitOps
 import React, { useMemo, useState } from "react";
 import { useApp } from "@/context/AppContext";
 import KpiCard from "@/components/KpiCard";
